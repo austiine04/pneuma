a329a4913f51c3e3b826850849cbe6fe4acf67a7
austiine <austiine04@gmail.com>
2015-03-21 17:24:47 +0300

    fixed typo

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pnuema-word_session'
+Rails.application.config.session_store :cookie_store, key: '_pneuma-word_session'
