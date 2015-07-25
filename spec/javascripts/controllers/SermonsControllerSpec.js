d7e2d56b21e0c6b88069ad3dbb6556962179bac5
austiine <austiine04@gmail.com>
2015-07-26 00:42:19 +0300

    refactor, reuse some code to clean up tests

--- spec/javascripts/controllers/SermonsControllerSpec.js
+++ spec/javascripts/controllers/SermonsControllerSpec.js
@@ -1,20 +1,18 @@
 describe('SermonsController', function () {
-    var scope, rootScope, deferred;
+    var scope, $rootScope, deferred;
     var mockSermonService = jasmine.createSpyObj('mockSermonservice', ['create']);
     var mockState = jasmine.createSpyObj('mockState', ['go']);
 
     beforeEach(module('pneumaApp'));
 
-    beforeEach(function () {
-        inject(function ($rootScope, $controller, $q) {
-            scope = $rootScope.$new();
-            rootScope = $rootScope;
-            deferred = $q.defer();
-            $controller('SermonsController', {$scope: scope, $state: mockState, SermonsService: mockSermonService});
-        });
+    beforeEach(inject(function (_$rootScope_, $controller, $q) {
+        scope = _$rootScope_.$new();
+        $rootScope = _$rootScope_;
+        deferred = $q.defer();
         deferred.resolve('sermon-id');
         mockSermonService.create.and.returnValue(deferred.promise);
-    });
+        $controller('SermonsController', {$scope: scope, $state: mockState, SermonsService: mockSermonService});
+    }));
 
     it('should have a sermon model', function () {
         expect(scope.data).toBeDefined();
@@ -23,22 +21,25 @@ describe('SermonsController', function () {
 
     describe('save', function () {
 
-        it('should user sermon service to save sermons', function () {
+        beforeEach(function () {
             scope.data = {title: 'title', preacher: 'preacher'};
             scope.save();
+        });
+
+        it('should user sermon service to save sermons', function () {
             expect(mockSermonService.create).toHaveBeenCalledWith(scope.data);
         });
 
         describe('success', function () {
+
             it('should redirect to sermon details page on success', function () {
-                scope.data = {title: 'title', preacher: 'preacher'};
-                scope.save();
-                rootScope.$digest();
+                $rootScope.$digest();
                 expect(mockState.go).toHaveBeenCalledWith('/sermons/sermon-id');
             });
 
             it('should display success message');
         });
+
         describe('error', function () {
             it('should remain at the sermon form when saving fails');
             it('should display error message');
