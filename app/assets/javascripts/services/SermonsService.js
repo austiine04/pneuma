134029f783844b2a8896fa01b3de50dc922eea30
austiine04 <austiine04@gmail.com>
2015-12-25 03:30:04 +1100

    vim seems to suggest a different indentation for the javascript code so i guess i'll use that

--- app/assets/javascripts/services/SermonsService.js
+++ app/assets/javascripts/services/SermonsService.js
@@ -1,35 +1,35 @@
 angular.module('sermonService', [])
-    .service('SermonsService', ['$http', '$q', function ($http, $q) {
-        var deferred = $q.defer();
-        var url = window.location.origin + '/api/v1/sermons';
+.service('SermonsService', ['$http', '$q', function ($http, $q) {
+  var deferred = $q.defer();
+  var url = window.location.origin + '/api/v1/sermons';
 
-        return {
-            create: function (sermon) {
-                $http.post(url, sermon).success(function (data, status, headers, config) {
-                    deferred.resolve(data.id);
-                }).error(function (error, status, headers, config) {
-                    deferred.reject(error);
-                });
-                return deferred.promise;
-            },
+  return {
+    create: function (sermon) {
+      $http.post(url, sermon).success(function (data, status, headers, config) {
+        deferred.resolve(data.id);
+      }).error(function (error, status, headers, config) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    },
 
-            all: function () {
-                $http.get(url).success(function (data, status, headers, config) {
-                    deferred.resolve(data);
-                }).error(function (error, status, headers, config) {
-                    deferred.reject(error);
-                });
-                return deferred.promise;
-            },
+    all: function () {
+      $http.get(url).success(function (data, status, headers, config) {
+        deferred.resolve(data);
+      }).error(function (error, status, headers, config) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    },
 
-            get: function (sermonId) {
-                var sermonUrl = url + '/' + sermonId;
-                $http.get(sermonUrl).success(function (data, status, headers, config) {
-                    deferred.resolve(data);
-                }).error(function (error, headers, status, config) {
-                    deferred.reject(error);
-                });
-                return deferred.promise;
-            }
-        };
-    }]);
+    get: function (sermonId) {
+      var sermonUrl = url + '/' + sermonId;
+      $http.get(sermonUrl).success(function (data, status, headers, config) {
+        deferred.resolve(data);
+      }).error(function (error, headers, status, config) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    }
+  };
+}]);
--- spec/javascripts/services/SermonsServiceSpec.js
+++ spec/javascripts/services/SermonsServiceSpec.js
@@ -1,96 +1,99 @@
 describe('SermonsService', function () {
-    var mockHttpBackend, SermonsService, $rootScope, deferred, url;
+  var mockHttpBackend, SermonsService, $rootScope, deferred, url;
 
-    beforeEach(module('pneumaApp'));
+  beforeEach(module('pneumaApp'));
 
-    beforeEach(inject(function (_SermonsService_, $httpBackend, _$rootScope_, $q) {
-        SermonsService = _SermonsService_;
-        mockHttpBackend = $httpBackend;
-        $rootScope = _$rootScope_;
-        deferred = $q.defer();
-        url = window.location.origin + '/api/v1/sermons';
-    }));
+  beforeEach(inject(function (_SermonsService_, $httpBackend, _$rootScope_, $q) {
+    SermonsService = _SermonsService_;
+    mockHttpBackend = $httpBackend;
+    $rootScope = _$rootScope_;
+    deferred = $q.defer();
+    url = window.location.origin + '/api/v1/sermons';
+  }));
 
-    function resolvePromises (stuff) {
+  function resolvePromises (stuff) {
+    $rootScope.$digest();
+    mockHttpBackend.flush();
+  }
+
+  describe('create', function () {
+    var sermon, returnedSermon;
+
+    beforeEach(function () {
+      sermon = {title: 'aaa-bbb-ccc', preacher: 'papa'};
+      returnedSermon = angular.extend(sermon, {id: 'returned-sermon'});
+    });
+
+    it('should POST sermon to the sermons api', function (){
+      mockHttpBackend.expectPOST(url, sermon).respond(201, returnedSermon);
+      SermonsService.create(sermon);
+      mockHttpBackend.flush();
+    });
+
+    it('should return sermon id on success', function () {
+      mockHttpBackend.whenPOST(url, sermon).respond(201, returnedSermon);
+      SermonsService.create(sermon).then(function(id) {
+        expect(id).toEqual(returnedSermon.id);
+      });
+      resolvePromises();
+    });
+
+    it('should return error on failure', function () {
+      mockHttpBackend.whenPOST(url, sermon).respond(500, 'Internal Server error');
+      SermonsService.create(sermon).then(function () {}, function (error) {
+        expect(error).toEqual('Internal Server error');
+      });
+      resolvePromises();
+    });
+  });
+
+  describe('all', function () {
+    describe('success', function () {
+      it('should return all sermons', function () {
+        var sermons = [{title: 'sermon-1'}, {title: 'sermon-2'}, {title: 'sermon-3'}];
+        mockHttpBackend.expectGET(url).respond(200, sermons);
+        SermonsService.all().then(function (sermons) {
+          expect(sermons.length).toEqual(3);
+        });
+        resolvePromises();
+      });
+    });
+
+    describe('error', function () {
+      it('should return error message', function () {
+        mockHttpBackend.expectGET(url).respond(400, 'An error occured');
         $rootScope.$digest();
-        mockHttpBackend.flush();
-    }
-
-    describe('create', function () {
-        var sermon, returnedSermon;
-
-        beforeEach(function () {
-            sermon = {title: 'aaa-bbb-ccc', preacher: 'papa'};
-            returnedSermon = angular.extend(sermon, {id: 'returned-sermon'});
+        SermonsService.all().then(function () {}, function (error) {
+          expect(error).toEqual('An error occured');
         });
-
-        it('should POST sermon to the sermons api', function (){
-            mockHttpBackend.expectPOST(url, sermon).respond(201, returnedSermon);
-            SermonsService.create(sermon);
-            mockHttpBackend.flush();
-        });
-
-        it('should return sermon id on success', function () {
-            mockHttpBackend.whenPOST(url, sermon).respond(201, returnedSermon);
-            SermonsService.create(sermon).then(function(id) {
-                expect(id).toEqual(returnedSermon.id);
-            });
-            resolvePromises();
-        });
-
-        it('should return error on failure', function () {
-            mockHttpBackend.whenPOST(url, sermon).respond(500, 'Internal Server error');
-            SermonsService.create(sermon).then(function () {}, function (error) {
-                expect(error).toEqual('Internal Server error');
-            });
-            resolvePromises();
-        });
-    });
-
-    describe('all', function () {
-        describe('success', function () {
-            it('should return all sermons', function () {
-                var sermons = [{title: 'sermon-1'}, {title: 'sermon-2'}, {title: 'sermon-3'}];
-                mockHttpBackend.expectGET(url).respond(200, sermons);
-                SermonsService.all().then(function (sermons) {
-                    expect(sermons.length).toEqual(3);
-                });
-                resolvePromises();
-            });
-        });
-
-        describe('error', function () {
-          it('should return error message', function () {
-              mockHttpBackend.expectGET(url).respond(400, 'An error occured');
-              $rootScope.$digest();
-              SermonsService.all().then(function () {}, function (error) {
-                  expect(error).toEqual('An error occured');
-              });
-              resolvePromises();
-          });
-        });
-    });
-
-    describe('get', function () {
-      var sermonUrl = url + '/1';
-      describe('success', function () {
-        it('should return the sermon', function () {
-          mockHttpBackend.expectGET(sermonUrl).respond(200, {title: 'title', preacher: 'preacher'});
-          SermonsService.get(1).then(function (sermon) {
-            expect(sermon.title).toEqual('title');
-            expect(sermon.preacher).toEqual('preacher');
-          });
-        });
+        resolvePromises();
       });
+    });
+  });
 
-      describe('error', function () {
-        it('should return error message from server', function () {
-          var errorMessage = 'Internal Server error';
-          mockHttpBackend.expectGET(sermonUrl).respond(500, errorMessage);
-          SermonsService.get(1).then(function () {}, function (error) {
-           expect(error).toEqual(errorMessage);
-          });
+  describe('get', function () {
+    var sermonUrl = url + '/1';
+
+    describe('success', function () {
+      it('should return the sermon', function () {
+        mockHttpBackend.expectGET(sermonUrl).respond(200, {title: 'title', preacher: 'preacher'});
+
+        SermonsService.get(1).then(function (sermon) {
+          expect(sermon.title).toEqual('title');
+          expect(sermon.preacher).toEqual('preacher');
         });
       });
     });
+
+    describe('error', function () {
+      it('should return error message from server', function () {
+        var errorMessage = 'Internal Server error';
+        mockHttpBackend.expectGET(sermonUrl).respond(500, errorMessage);
+
+        SermonsService.get(1).then(function () {}, function (error) {
+          expect(error).toEqual(errorMessage);
+        });
+      });
+    });
+  });
 });
