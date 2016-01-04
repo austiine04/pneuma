bc24e213a77bf014a0c3703292fced261923509f
austiine04 <austiine04@gmail.com>
2016-01-04 11:51:10 +1100

    make button purple and align progress bar

--- app/assets/javascripts/directives/Purple.js
+++ app/assets/javascripts/directives/Purple.js
@@ -1,10 +1,13 @@
+//TODO: rename this directive to some meaningful name
 angular.module('createSermonDirective', [])
 .directive('purpleWidget', function () {
   return {
     restrict: 'A',
     link: function (scope, element, attributes) {
-      element.children()[0].remove();
-      element.children()[1].style.display = '';
+      element.children()[0].style.float = 'left';
+      element.children()[0].classList.remove('btn-primary');
+      element.children()[1].classList.add('col-md-10');
+      element.children()[1].classList.add('align-progress-bar');
     }
   };
 });
