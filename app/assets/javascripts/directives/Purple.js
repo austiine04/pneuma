38360199a7f30061e13d56b0f2bdd4879b8f44ea
austiine04 <austiine04@gmail.com>
2016-01-04 12:02:04 +1100

    refactor - encapsulate logic into descriptive private functions and move sermons related css out to a seperate file

--- app/assets/javascripts/directives/Purple.js
+++ app/assets/javascripts/directives/Purple.js
@@ -1,13 +1,22 @@
 //TODO: rename this directive to some meaningful name
 angular.module('createSermonDirective', [])
 .directive('purpleWidget', function () {
+
+  var styleButton = function (button) {
+    button.style.float = 'left';
+    button.classList.remove('btn-primary');
+  };
+
+  var styleProgressBar = function (progressBar) {
+    progressBar.classList.add('col-md-10');
+    progressBar.classList.add('align-progress-bar');
+  };
+
   return {
     restrict: 'A',
     link: function (scope, element, attributes) {
-      element.children()[0].style.float = 'left';
-      element.children()[0].classList.remove('btn-primary');
-      element.children()[1].classList.add('col-md-10');
-      element.children()[1].classList.add('align-progress-bar');
+      styleButton(element.children()[0]);
+      styleProgressBar(element.children()[1]);
     }
   };
 });
