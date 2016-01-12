0eaffe50c6d2ee8f1b164c23384d94ecaee0fb81
austiine04 <austiine04@gmail.com>
2016-01-12 11:30:36 +1100

    bug fix, promises seem to cache their and so this was making the app behave weirdly

--- app/assets/javascripts/services/SermonsService.js
+++ app/assets/javascripts/services/SermonsService.js
@@ -1,10 +1,10 @@
 angular.module('sermonService', [])
 .service('SermonsService', ['$http', '$q', function ($http, $q) {
-  var deferred = $q.defer();
   var url = window.location.origin + '/api/v1/sermons';
 
   return {
     create: function (sermon) {
+      var deferred = $q.defer();
       $http.post(url, sermon).success(function (data, status, headers, config) {
         deferred.resolve(data.id);
       }).error(function (error, status, headers, config) {
@@ -14,6 +14,7 @@ angular.module('sermonService', [])
     },
 
     all: function () {
+      var deferred = $q.defer();
       $http.get(url).success(function (data, status, headers, config) {
         deferred.resolve(data);
       }).error(function (error, status, headers, config) {
@@ -23,6 +24,7 @@ angular.module('sermonService', [])
     },
 
     get: function (sermonId) {
+      var deferred = $q.defer();
       var sermonUrl = url + '/' + sermonId;
       $http.get(sermonUrl).success(function (data, status, headers, config) {
         deferred.resolve(data);
