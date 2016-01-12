494d1cbf4cfc11a96d180226629a2bd7c7343ffe
austiine04 <austiine04@gmail.com>
2016-01-12 12:11:14 +1100

    authenticate urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
-  get '/dashboard', to: 'dashboard#index', as: 'dashboard'
-  get '/getS3Options', to: 'dashboard#s3_access_token', as: 'access_token'
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
+  authenticate :user do
+    get '/dashboard', to: 'dashboard#index', as: 'dashboard'
+    get '/getS3Options', to: 'dashboard#s3_access_token', as: 'access_token'
+  end
+
   devise_for :users
 
   namespace :api do
     namespace :v1 do
-      resources :sermons, only: [:create, :index, :show]
+      authenticate :user do
+        resources :sermons, only: [:create]
+      end
+
+      resources :sermons, only: [:index, :show]
     end
   end
 end
